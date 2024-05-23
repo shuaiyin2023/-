b7958d053cfb62cb231e7a6786cfe91ca04dfb7a
shuaiyin <2018209921@qq.com>
2024-05-23 11:22:53 +0800

    新增僵尸移动和清理各个精灵组的边界数据

--- pygame/植物大战僵尸.py
+++ pygame/植物大战僵尸.py
@@ -4,7 +4,8 @@ from pygame.locals import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("植物大战僵尸")
 
 """ 植物类 """
@@ -19,13 +20,20 @@ class Plants(pygame.sprite.Sprite):
         self.last_shot = pygame.time.get_ticks()
         self.shoot_delay = 1000  # 子弹发射间隔（毫秒）
 
+    # 循环发射子弹
     def update(self):
         now = pygame.time.get_ticks()
         if now - self.last_shot > self.shoot_delay:
             self.last_shot = now
             bullet = Bullet(self.rect.center)
-            all_sprites.add(bullet)
-            bullets.add(bullet)
+            all_plants_sprites.add(bullet)  # 将循环生成的子弹添加到植物精灵组中是为了将与植物相关的子弹循环生成，实现循环发射的效果
+            bullets.add(bullet)  # 这里将子弹加入子弹精灵组中是为了在子弹精灵组中专门处理子弹的移动、碰撞等逻辑
+
+        ''' 因为在Plants的循环发射子弹中，每新增一颗子弹，都会在植物精灵组中也添加一个子弹精灵，
+                如果数据过大占用过多内存影响性能，所以当子弹超出边界，将其从精灵组中删除 '''
+        for _sprite in all_plants_sprites:
+            if _sprite.rect.right >= WIDTH:
+                _sprite.kill()
 
 
 """ 向日葵类 """
@@ -46,7 +54,7 @@ class SunFlowers(Plants):
         self.image_delay = 10  # 图像切换延迟
         self.current_frame = 0  # 当前图片
 
-    # 图片的动态效果处理
+    # 图片的切割处理
     def deal_image(self):
         for image_width in range(0, self.image.get_width(), self.frame_width):
             for image_height in range(0, self.image.get_height(), self.frame_height):
@@ -55,7 +63,7 @@ class SunFlowers(Plants):
                 sub = self.image.subsurface(pygame.Rect(image_width, image_height, self.frame_width, self.frame_height))
                 self.frames.append(sub)
 
-    # 实现图片的动态效果
+    # 轮播图片
     def dynamic_effect(self):
         # 设置动态效果的时间频率
         self.image_timer += 1
@@ -69,6 +77,9 @@ class SunFlowers(Plants):
         # 在屏幕上绘制当前帧的小图
         screen.blit(self.image, self.rect)
 
+    # def update(self):
+    #     pass
+
 
 """ 豌豆射手类 """
 
@@ -78,7 +89,7 @@ class Peashooter(Plants):
     def __init__(self, pos):
         super().__init__(pos)
         self.image = pygame.image.load('./images/Peashooter.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (82, 77))
+        self.image = pygame.transform.scale(self.image, (50, 50))
 
 
 """ 子弹类 """
@@ -92,14 +103,34 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=pos)
         self.speed = 5
 
+    # 子弹移动及是否超出范围
     def update(self):
         self.rect.x += self.speed
-        if self.rect.bottom < 0:
+        if self.rect.right < 0:
             self.kill()
 
 
-all_sprites = pygame.sprite.Group()
-bullets = pygame.sprite.Group()
+""" 僵尸类 """
+
+
+class Zombies(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.image.load('./images/zombie.png').convert_alpha()  # 僵尸
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect(center=pos)
+        self.speed = 1
+
+    # 僵尸移动
+    def update(self):
+        self.rect.x -= self.speed
+        if self.rect.left < 0:
+            self.kill()
+
+
+all_plants_sprites = pygame.sprite.Group()  # 所有植物类精灵
+all_zombie_sprites = pygame.sprite.Group()  # 所有僵尸类精灵
+bullets = pygame.sprite.Group()  # 所有植物类子弹精灵
 
 clock = pygame.time.Clock()
 running = True
@@ -110,21 +141,35 @@ while running:
         if event.type == QUIT:
             running = False
         elif event.type == MOUSEBUTTONDOWN:
-            # plant = Plant(event.pos)
-            plant = SunFlowers(event.pos)
-            plant.deal_image()  # 调用 deal_image() 方法来切分图像
-            all_sprites.add(plant)
-    for sprite in all_sprites:
+            if event.button == 1:
+                # plant = Plant(event.pos)
+                # plant = Peashooter(event.pos)
+                plant = SunFlowers(event.pos)
+                plant.deal_image()  # 调用 deal_image() 方法来切分图像
+                all_plants_sprites.add(plant)
+            if event.button == 3:
+                zombie = Zombies(event.pos)
+                all_zombie_sprites.add(zombie)
+
+    # 这个for循环用于展示向日葵的动态效果
+    for sprite in all_plants_sprites:
         if isinstance(sprite, SunFlowers):
             sprite.dynamic_effect()
-    all_sprites.update()
 
+    # 子弹超出边界则将其销毁
     for bullet in bullets:
-        if bullet.rect.top <= 0:
+        if bullet.rect.right <= 0:
             bullet.kill()
+    all_plants_sprites.update()  # 将精灵组中的所有精灵对象的状态和属性及时更新
+    all_zombie_sprites.update()  # 将精灵组中的所有精灵对象的状态和属性及时更新
+
+    print("僵尸: \n", all_zombie_sprites, len(all_zombie_sprites))
+    print("植物: \n", all_plants_sprites, len(all_plants_sprites))
+    print("子弹: \n", bullets, len(bullets))
 
     screen.fill((0, 0, 0))
-    all_sprites.draw(screen)
+    all_plants_sprites.draw(screen)  # 将所有精灵组中的精灵对象绘制到屏幕上
+    all_zombie_sprites.draw(screen)  # 将所有精灵组中的精灵对象绘制到屏幕上
 
     pygame.display.flip()
 
